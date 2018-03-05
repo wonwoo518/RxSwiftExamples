824099d10b1ef895f785e4fba490411267be5ed3
1002237 <wonwoo@sk.com>
2018-03-05 15:35:04 +0900

    -

--- login/RxSwiftTutorial/RxSampleTableViewController.swift
+++ login/RxSwiftTutorial/RxSampleTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  RxSampleTable.swift
+//  RxSampleTableViewController.swift
 //  RxSwiftTutorial
 //
 //  Created by 1002237 on 2018. 3. 5..
@@ -7,9 +7,27 @@
 //
 
 import UIKit
-
-class RxSampleTalble : UITableViewController{
+var navi:UINavigationController? = nil
+class RxSampleTableViewController : UITableViewController{
     override func viewDidLoad() {
+        navi = self.navigationController
     }
     
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "rxcell", for: indexPath) as UITableViewCell
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+}
+
+class RxCell : UITableViewCell{
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var detailButton: UIButton!
+    @IBAction func onButton(_ sender: Any) {
+        let v = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginViewController")
+        navi?.pushViewController(v, animated: true)
+    }
 }
